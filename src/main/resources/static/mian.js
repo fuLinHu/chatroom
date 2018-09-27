1e7972342462252a99ded7fb7cb6b1b93adbf70f
fuLinHu <1205668006@qq.com>
2018-09-28 01:44:25 +0800

    完成ui设计

--- src/main/resources/static/mian.js
+++ src/main/resources/static/mian.js
@@ -1,10 +1,10 @@
 $(function(){
+    var webSocket = new WebSocket("ws://localhost:9999/websocket");
     $(".send").on("click",function(){
         var message= $(".message").val();
         message=$.trim(message);
         if(message){
-            $(".body").append("<span class='me'><span>"+message+"</span></span>");
-            $(".body")[0].scrollTop=$(".body")[0].scrollHeight;
+            webSocket.send(message);
         }
     })
     $(".message").on("keyup",function(){
@@ -16,4 +16,22 @@ $(function(){
             $(".send").removeClass("active");
         }
     })
+
+    webSocket.onopen = function(event){
+        console.log("连接成功");
+        console.log(event);
+    };
+    webSocket.onerror = function(event){
+        console.log("连接失败");
+        console.log(event);
+    };
+    webSocket.onclose = function(event){
+        console.log("Socket连接断开");
+        console.log(event);
+    };
+    webSocket.onmessage = function(event){
+        $(".body").append("<span class='me'><span>"+event.data+"</span></span>");
+        $(".body")[0].scrollTop=$(".body")[0].scrollHeight;
+    }
+
 })
